e2fdf9b75123c07ff00d513ac0a4c3609183e02f
HuuTri130401 <trith130401@gmail.com>
2023-11-05 11:29:07 +0700

    Add initialStatuses and show task in board status

--- src/components/drag-drop/DragDrop.jsx
+++ src/components/drag-drop/DragDrop.jsx
@@ -1,52 +1,55 @@
-import React from 'react'
-import { DragDropContext, Droppable } from 'react-beautiful-dnd'
-import { useDispatch } from 'react-redux'
-import ListTask from '../list-task/ListTask'
-import { updateStatusTaskThunk } from '../../redux/thunk/taskThunk'
+import React from 'react';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { useDispatch } from 'react-redux';
+import ListTask from '../list-task/ListTask';
+import { updateStatusTaskThunk } from '../../redux/thunk/taskThunk';
 
 const DragDrop = ({ projectDetail }) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const handleDrag = (result) => {
+    const { projectId, id } = JSON.parse(result.draggableId);
+    const { source, destination } = result;
 
-    const handleDrag = (result) => {
-      let { projectId, id } = JSON.parse(result.draggableId);
-      let { source, destination } = result;
-      if (!destination) {
-          return;
-      } else if (source.index === destination.index && source.droppableId === destination.droppableId) {
-          return;
-      }
-      
-      dispatch(updateStatusTaskThunk({
-          taskId: id,
-          statusId: destination.droppableId,
-          projectId // Đảm bảo projectId đã được truyền vào hàm
-      }))
-  }
+    if (!destination) {
+      return;
+    } else if (source.index === destination.index && source.droppableId === destination.droppableId) {
+      return;
+    }
+    
+    dispatch(updateStatusTaskThunk({
+      taskId: id,
+      statusId: destination.droppableId,
+      projectId,
+    }));
+  };
+
+  const renderTaskList = () => {
+    const project = projectDetail.projectById;
+    const initialStatuses = ['Backlog', 'Selected for Development', 'In Progress', 'Doing', 'Done'];
   
-    const renderTaskList = () => {
-        const project = projectDetail.projectById;
-        // Tạo danh sách các trạng thái duy nhất
-        const uniqueStatuses = [...new Set(project?.todos?.map((task) => task.status))];
-        return uniqueStatuses.map((status, index) => {
-          // Lọc danh sách công việc theo trạng thái
-          const filteredTasks = project.todos.filter((task) => task.status === status);
-          return (
-            <Droppable key={status} droppableId={status}>
-              {(provider, snapshot) => {
-                return <ListTask key={index} taskList={{ status }} provider={provider} project={project} filteredTasks={filteredTasks} />;
-              }}
-            </Droppable>
-          );
-        });
-      };
-      
-    return (
-        <div className="list__board">
-            <DragDropContext onDragEnd={handleDrag}>
-                {renderTaskList()}
-            </DragDropContext>
-        </div>
-    )
-}
+    if (project && project.todos) {
+      return initialStatuses.map((status, index) => {
+        const filteredTasks = project.todos.filter((task) => task.status === status);
+        return (
+          <Droppable key={status} droppableId={status}>
+            {(provider, snapshot) => {
+              return <ListTask key={index} taskList={{ status }} provider={provider} project={project} filteredTasks={filteredTasks} />;
+            }}
+          </Droppable>
+        );
+      });
+    } else {
+      return null; // or handle the case where project or project.todos is not available
+    }
+  };
+  
+  return (
+    <div className="list__board">
+      <DragDropContext onDragEnd={handleDrag}>
+        {renderTaskList()}
+      </DragDropContext>
+    </div>
+  );
+};
 
 export default DragDrop;
